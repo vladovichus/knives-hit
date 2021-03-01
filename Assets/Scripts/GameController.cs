c98833e5b1b358cd9d23de0589d32bbd0b4b3f06
Vlad <vladovichus@gmail.com>
2021-03-02 00:26:22 +0300

    initial commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(GameUI))]
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance { get; private set; }
+
+    [SerializeField] private int knifeCount;
+
+    [SerializeField] [Header("Knife Spawning")]
+    private Vector2 knifeSpawnPosition;
+
+    [SerializeField] private GameObject knifeObject;
+
+    public GameUI GameUI { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+        GameUI = GetComponent<GameUI>();
+    }
+
+
+    private void Start()
+    {
+        GameUI.SetInitialDisplayedKnifeCount(knifeCount);
+        SpawnKnife();
+    }
+
+    public void OnSuccessfulKnifeHit()
+    {
+        if (knifeCount > 0)
+        {
+            SpawnKnife();
+        }
+        else
+        {
+            StartGameOverSequence(true);
+        }
+    }
+
+    private void SpawnKnife()
+    {
+        knifeCount--;
+        Instantiate(knifeObject, knifeSpawnPosition, Quaternion.identity);
+    }
+
+    public void StartGameOverSequence(bool win)
+    {
+        StartCoroutine("GameOverSequenceCoroutine", win);
+    }
+
+    private IEnumerator GameOverSequenceCoroutine(bool win)
+    {
+        if (win)
+        {
+            yield return new WaitForSecondsRealtime(0.3f);
+            RestartGame();
+        }
+        else
+        {
+            GameUI.ShowRestartButton();
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+    }
+}
